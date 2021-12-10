0afe10a94d8a5ddee4384bd054bec49ba2354eb4
Zhao Zhu <John_Chu@outlook.com>
2021-12-10 14:30:31 -0600

    added support for pruning and clustering weights

--- kerasmodels/SpleeterTrainKerasBase.py
+++ kerasmodels/SpleeterTrainKerasBase.py
@@ -10,7 +10,10 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 import tensorflow.keras.backend as K
 
-audio_path = '../musdb_dataset/'
+import tensorflow_model_optimization as tfmot
+import tempfile
+
+audio_path = 'musdb_dataset'
 config_path = "config/musdb_config.json"
 INIT_LR = 1e-4
 opt = Adam(INIT_LR)
@@ -92,6 +95,87 @@ def dice_coefficient(y_true, y_pred):
 	return numerator / (denominator + tf.keras.backend.epsilon())
 
 
+def trainClusteredModelOverEpochs(noOfEpochs=20, saveModelEvery=5, startEpochVal=0, modelPath=None, learningRate=0):
+
+	global opt
+	if(learningRate==0):
+		learningRate = INIT_LR
+	# if there is no specific model checkpoint supplied, then initialize
+	# the network and compile the model
+	if modelPath is None:
+		print("[INFO] compiling model...")
+		model = getUnetModel("vocals")
+		model.compile(loss=custom_loss, optimizer=opt,
+			metrics=[dice_coefficient])
+	# otherwise, we're using a checkpoint model
+	else:
+		# load the checkpoint from disk
+		print("[INFO] loading {}...".format(modelPath))
+		model = load_model(modelPath, custom_objects={"loss":custom_loss, "metrics":dice_coefficient}, compile=False)
+		model.compile(loss=custom_loss, optimizer=opt,
+					  metrics=[dice_coefficient])
+
+		K.set_value(model.optimizer.lr, learningRate)
+		print("[INFO] new learning rate: {}".format(
+			K.get_value(model.optimizer.lr)))
+
+	def apply_clustering_to_dense(layer):
+		if isinstance(layer, tf.keras.layers.Conv2D):
+			return cluster_weights(layer, **clustering_params)
+		return layer
+	
+	cluster_weights = tfmot.clustering.keras.cluster_weights
+	CentroidInitialization = tfmot.clustering.keras.CentroidInitialization
+
+	clustering_params = {
+	'number_of_clusters': 16,
+	'cluster_centroids_init': CentroidInitialization.LINEAR
+	}
+
+	clustered_model = tf.keras.models.clone_model(
+		model,
+		clone_function=apply_clustering_to_dense,
+	)
+
+	# Cluster a whole model
+	clustered_model.summary()
+
+	clustered_model.compile(loss=custom_loss, optimizer=opt,
+					  metrics=[dice_coefficient])
+
+
+
+	# build the path to the training plot and training history
+	plotPath = "./kerasmodels/plots/resnet_fashion_mnist.png"
+	jsonPath = "./kerasmodels/plots/resnet_fashion_mnist.json"
+
+	# construct the set of callbacks
+	callbacks = [
+		EpochCheckpoint(checkPointPath, every=saveModelEvery,
+			startAt=startEpochVal),
+		TrainingMonitor(plotPath,
+			jsonPath=jsonPath,
+			startAt=startEpochVal)]
+
+
+	input_ds = get_training_dataset(params, audio_adapter, audio_path )
+	test_ds = get_validation_dataset(params, audio_adapter, audio_path)
+
+	clustered_model.fit_generator(
+		input_ds,
+		validation_data=test_ds,
+		steps_per_epoch=64,
+		epochs=noOfEpochs,
+		callbacks=callbacks,
+		verbose=1)
+
+	final_model = tfmot.clustering.keras.strip_clustering(clustered_model)
+	model.save('./clustered_model')
+
+
+	print(100)
+
+
 def trainModelOverEpochs(noOfEpochs=20, saveModelEvery=5, startEpochVal=0, modelPath=None, learningRate=0):
 
 	global opt
@@ -134,6 +218,74 @@ def trainModelOverEpochs(noOfEpochs=20, saveModelEvery=5, startEpochVal=0, model
 	test_ds = get_validation_dataset(params, audio_adapter, audio_path)
 
 	model.fit_generator(
+		input_ds,
+		validation_data=test_ds,
+		steps_per_epoch=64,
+		epochs=noOfEpochs,
+		callbacks=callbacks,
+		verbose=1)
+
+	print(100)
+
+def trainPrunedModelOverEpochs(noOfEpochs=20, saveModelEvery=5, startEpochVal=0, modelPath=None, learningRate=0):
+
+	global opt
+	if(learningRate==0):
+		learningRate = INIT_LR
+	# if there is no specific model checkpoint supplied, then initialize
+	# the network and compile the model
+	if modelPath is None:
+		print("[INFO] compiling model...")
+		model = getUnetModel("vocals")
+		model.compile(loss=custom_loss, optimizer=opt,
+			metrics=[dice_coefficient])
+	# otherwise, we're using a checkpoint model
+	else:
+		# load the checkpoint from disk
+		print("[INFO] loading {}...".format(modelPath))
+		model = load_model(modelPath, custom_objects={"loss":custom_loss, "metrics":dice_coefficient}, compile=False)
+		model.compile(loss=custom_loss, optimizer=opt,
+					  metrics=[dice_coefficient])
+
+		K.set_value(model.optimizer.lr, learningRate)
+		print("[INFO] new learning rate: {}".format(
+			K.get_value(model.optimizer.lr)))
+
+	def apply_pruning_to_dense(layer):
+		if isinstance(layer, tf.keras.layers.Conv2D):
+			return tfmot.sparsity.keras.prune_low_magnitude(layer)
+		return layer
+
+	model_for_pruning = tf.keras.models.clone_model(
+		model,
+		clone_function=apply_pruning_to_dense,
+	)
+
+	model_for_pruning.summary()
+
+
+	# build the path to the training plot and training history
+	plotPath = "./kerasmodels/plots/resnet_fashion_mnist.png"
+	jsonPath = "./kerasmodels/plots/resnet_fashion_mnist.json"
+
+	log_dir = tempfile.mkdtemp()
+	# construct the set of callbacks
+	callbacks = [
+		EpochCheckpoint(checkPointPath, every=saveModelEvery,
+			startAt=startEpochVal),
+		TrainingMonitor(plotPath,
+			jsonPath=jsonPath,
+			startAt=startEpochVal),
+		tfmot.sparsity.keras.UpdatePruningStep(),
+		# Log sparsity and other metrics in Tensorboard.
+		tfmot.sparsity.keras.PruningSummaries(log_dir=log_dir)
+		]
+
+
+	input_ds = get_training_dataset(params, audio_adapter, audio_path )
+	test_ds = get_validation_dataset(params, audio_adapter, audio_path)
+
+	model_for_pruning.fit_generator(
 		input_ds,
 		validation_data=test_ds,
 		steps_per_epoch=64,
